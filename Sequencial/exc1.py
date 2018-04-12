692c766b73cb9a89a051dbbc8f2dc614213367a9
Fabiojs <fabiojfl@gmail.com>
2018-04-12 15:47:53 -0300

    config iniciais

---
+++ Sequencial/exc1.py
@@ -0,0 +1,15 @@
+#1.Faca um programa que receba quatro numeros inteiros, calcule e mostre a soma desses numeros.
+
+print("SEQUENCIAL*********************************************")
+print("Programa*1*********************************************")
+print("*****Adcione 4 numeros(Inteiros) e veja a soma deles***")
+
+num1 = input("Adicione o primeiro número \n" )
+num2 = input("Adicione o segundo número \n" )
+#num2 = input("Adicione o segundo número ")
+#num3 = input("Adicione o terceiro número ")
+#num4 = input("Adicione o quarto número ")
+
+print(int(num1) +int(num2))
+
+
