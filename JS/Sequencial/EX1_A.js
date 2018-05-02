656c6739bfe53b270aa9744d873a3c74c94e476e
Fabiojs <fabiojfl@gmail.com>
2018-05-02 09:17:47 -0300

    start fundamentos da Programação JavaScript

---
+++ JS/Sequencial/EX1_A.js
@@ -0,0 +1,21 @@
+//1.Faca um programa que receba quatro numeros inteiros, calcule e mostre a soma desses numeros.
+
+alert('Adicione 4 numeros para obter a soma entre eles')
+
+num1 = prompt('Adione o primeiro numero')
+num2 = prompt('Adione o segundo numero')
+num3 = prompt('Adione o terceiro numero')
+num4 = prompt('Adione o quarto numero')
+
+intNum1 = parseInt(num1)
+intNum2 = parseInt(num2)
+intNum3 = parseInt(num3)
+intNum4 = parseInt(num4)
+
+res = intNum1 + intNum2 + intNum3 + intNum4
+
+console.log('A soma dos 4 numeros é: '+res)
+
+
+
+
